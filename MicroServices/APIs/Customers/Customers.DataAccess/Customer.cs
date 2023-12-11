fecb7655b660148705056d139f89d22595c48ff4
giisen <kristerekelund@me.com>
2023-12-11 11:56:44 +0100

    customers

---
+++ MicroServices/APIs/Customers/Customers.DataAccess/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common.Interfaces;
+using MongoDB.Bson;
+
+namespace Customers.DataAccess;
+
+public class Customer : IEntity<ObjectId>
+{
+    public ObjectId Id { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string Country { get; init; }
+    public string City { get; init; }
+    public string StreetAddress { get; init; }
+    public int PostalCode { get; init; }
+}
