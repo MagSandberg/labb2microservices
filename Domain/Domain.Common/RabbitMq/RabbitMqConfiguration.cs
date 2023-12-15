286df67167564a76b280b417f9c9d7798bdca039
MagnusSandbergITHS <magnus.sandberg@iths.se>
2023-12-15 10:02:54 +0100

    RabbitMQ init

---
+++ Domain/Domain.Common/RabbitMq/RabbitMqConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Common.RabbitMq;
+
+public class RabbitMqConfiguration
+{
+    public string HostName { get; init; }
+    public string Username { get; init; }
+    public string Password { get; init; }
+}
